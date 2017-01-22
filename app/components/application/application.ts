3e8b5fa74bda58262c0d4c5120421a3243df2be1
colinbinion <colin.binion@me.com>
2017-01-21 17:26:17 -0700

    application components/html/css/ts

---
+++ app/components/application/application.ts
@@ -0,0 +1,18 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+import {Product, ProductService} from '../../services/product-service';
+
+@Component({
+  selector: 'auction-application',
+  templateUrl: 'app/component/application/application.html',
+  styleUrls: ['app/components/application/application.css'],
+  encapsulation: ViewEncapsulation.None
+})
+
+export default class ApplicationComponent {
+
+  products: Array<Product> = [];
+
+  constructor(private productService: ProductService) {
+    this.products = this.productService.getProducts();
+  }
+}
