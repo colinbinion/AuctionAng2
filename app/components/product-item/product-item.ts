bdb90ec6271ca0fb8afc64032af4040a0865c2b4
colinbinion <colin.binion@me.com>
2017-01-23 06:01:08 -0700

    fixed component render issue

--- app/components/product-item/product-item.ts
+++ app/components/product-item/product-item.ts
@@ -4,7 +4,7 @@ import {Product} from '../../services/product-service';
 @Component({
   selector: 'auction-product-item',
   styleUrls: ['app/components/product-item/product-item.css'],
-  templateUrl: 'app/components/product-item/product-item.html'
+  templateUrl: 'app/components/product-item/product-item.html',
 })
 export default class ProductItemComponent {
   @Input() product: Product;
--- app/components/stars/stars.ts
+++ app/components/stars/stars.ts
@@ -1,18 +1,22 @@
-import {Component,  Input, OnInit} from '@angular/core'; // <1>
+// tag::annotations[]
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   templateUrl: 'app/components/stars/stars.html',
-  styles: [` .starrating { color: #d17581; }`],
-  selector: 'auction-stars'
+  selector: 'auction-stars',
+  inputs: ['rating', 'count']
 })
+// end::annotations[]
+// tag::class[]
 export default class StarsComponent implements OnInit {
-  @Input() count: number = 5;
-  @Input() rating: number = 0;
+  count: number = 5;
+  rating: number = 0;
   stars: boolean[] = [];
 
-  ngOnInit() { // <4>
+  ngOnInit() {
     for (let i = 1; i <= this.count; i++) {
       this.stars.push(i > this.rating);
     }
   }
 }
+// end::class[]
